7404bc100219d5cd9219ca5f78fec178b2166bed
Adrian Olejniczak <adrian.olejniczak@infinite.pl>
2017-08-28 17:03:37 +0200

    Add Commands

---
+++ Core.CQRS/Commands/CommandDispatcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Core.CQRS.Commands.Interfaces;
+using System;
+using Core.IoC.Interfaces;
+
+namespace Core.CQRS.Commands
+{
+    public class CommandDispatcher : ICommandDispatcher
+    {
+        private IIoCContainer _context;
+
+        public CommandDispatcher(IIoCContainer context)
+        {
+            _context = context;
+        }
+
+        public async Task DispatchAsync<T>(T command) where T : ICommand
+        {
+            if (command == null)
+                throw new ArgumentNullException(nameof(command),"Command can not be null.");
+
+            ICommandHandler<T> handler = _context.Resolve<ICommandHandler<T>>();
+            await handler.HandleAsync(command);
+        }
+    }
+}
---
+++ Core.CQRS/Commands/Interfaces/ICommandDispatcher.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Core.CQRS.Commands.Interfaces
+{
+    public interface ICommandDispatcher
+    {
+        Task DispatchAsync<T>(T command) where T : ICommand;
+    }
+}
